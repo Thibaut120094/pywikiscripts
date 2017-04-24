a06a9ad4f319803bd574cc84668a148ccec07fe7
Thibaut <Thibaut120094@users.noreply.github.com>
2017-04-24 17:15:09 +0200

    Update colgate.py

--- colgate.py
+++ colgate.py
@@ -9,7 +9,7 @@ category = pywikibot.Category(site, u"japonais")
 
 for page in pywikibot.site.APISite.categorymembers(site, category, namespaces=[0]):
    title = page.title()
-   regex1 = ur"^\[\[(\w{1,6}|zh-min-nan)\:%s\]\]\s*(\n|$)" % title
+   regex1 = ur"^\[\[(\w{1,6}|zh-min-nan|roa-rup)\:%s\]\]\s*(\n|$)" % title
    pattern = re.findall(regex1, page.text, flags=re.MULTILINE)
    print title
    if pattern:
