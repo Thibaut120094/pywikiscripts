3079ed46c35f30ca7ce26e003653517cee5a1c24
Thibaut <Thibaut120094@users.noreply.github.com>
2017-04-25 18:31:33 +0200

    Update colgate.py

--- colgate.py
+++ colgate.py
@@ -11,7 +11,6 @@ for page in pywikibot.site.APISite.categorymembers(site, category, namespaces=[0
    title = page.title()
    regex1 = ur"^\[\[(\w{1,6}|zh-min-nan|roa-rup)\:%s\]\]\s*(\n|$)" % re.escape(title)
    pattern = re.findall(regex1, page.text, flags=re.MULTILINE)
-   print title
    if pattern:
      page.text = re.sub(regex1, "", page.text, flags=re.MULTILINE)
      page.save(u"Retrait des liens interlangues qui sont maintenant gérés automatiquement par [[:mw:Extension:Cognate]].")
