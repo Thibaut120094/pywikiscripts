4c70cc8a264f0726f8f3f9f38699204f2271eda0
Thibaut <Thibaut120094@users.noreply.github.com>
2017-05-04 23:32:58 +0200

    add ignorelist

--- colgate.py
+++ colgate.py
@@ -5,10 +5,13 @@ import pywikibot
 import re
 
 site = pywikibot.Site(u'fr', u'wiktionary', u'RobokoBot')
-category = pywikibot.Category(site, u"français")
+category = pywikibot.Category(site, u"anglais")
+list = [u"’", u"\'", u"…", u"..."]
 
 for page in pywikibot.site.APISite.categorymembers(site, category, namespaces=[0]):
-   title = page.title()
+ title = page.title()
+ if title not in list:
+   print title
    regex1 = ur"^\[\[(\w{1,6}|zh-min-nan|roa-rup)\:%s\]\]\s*(\n|$)" % re.escape(title)
    pattern = re.findall(regex1, page.text, flags=re.MULTILINE)
    if pattern:
